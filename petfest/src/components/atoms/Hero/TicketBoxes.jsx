7c8bb4f2dcc05a5ec7240da9e13c8af9674c36af
Defri Indra Mahardika <defrindr@gmail.com>
2025-03-16 09:58:01 +0700

    refactor TicketBoxes component to use ButtonStyled for improved code reusability and maintainability

--- petfest/src/components/atoms/Hero/TicketBoxes.jsx
+++ petfest/src/components/atoms/Hero/TicketBoxes.jsx
@@ -1,5 +1,43 @@
 import { Button } from "@/components/ui/button";
 import { useMemo } from "react";
+import { twMerge } from "tailwind-merge";
+
+function ButtonStyled({ isMobile = false, label = "", limitWidth = false }) {
+  const classMerge = twMerge(
+    "bg-[#EC497F] px-4 py-2 sm:px-5 sm:py-2 md:px-8  mx-auto shadow-md border-[1px] md:border-2 border-black",
+    limitWidth ? "max-w-[60%] sm:max-w-[250px]" : "w-full flex items-center justify-center flex-col",
+    isMobile ? "rounded-2xl" : "rounded-3xl"
+  );
+
+  return (
+    <div
+      className={classMerge}
+      style={{
+        boxShadow: isMobile ? "1px 1px 0 #000" : "2px 2px 0 #000",
+      }}
+    >
+      <span
+        className="text-lg sm:text-xl md:text-3xl font-bold text-white block text-center"
+        style={{
+          textShadow: "2px 2px 0 #3F1508",
+        }}
+      >
+        {label}
+      </span>
+      <div className="mt-1 text-center px-1">
+        <Button
+          className="text-xs sm:text-sm font-bold bg-yellow-400 hover:bg-yellow-500 rounded-md px-2 py-1 font-crunch-chips border border-black"
+          style={{
+            textShadow: "1px 1px 0 #3F1508",
+            boxShadow: "1px 1px 0 #000",
+          }}
+        >
+          CLICK HERE
+        </Button>
+      </div>
+    </div>
+  );
+}
 
 function TicketBoxes({ isMobile = false, view }) {
   // Deteksi ukuran layar untuk responsivitas
@@ -26,93 +64,22 @@ function TicketBoxes({ isMobile = false, view }) {
         className={`${twClass} z-20 flex flex-col items-center space-y-3 sm:space-y-4 font-crunch-chips px-4 sm:px-6`}
       >
         {/* BCA Rp1 button */}
-        <div
-          className="bg-[#EC497F] rounded-lg px-4 py-2 sm:px-5 sm:py-2 md:px-8 shadow-md max-w-[200px] sm:max-w-[250px] border-[1px] sm:border-2 border-black"
-          style={{
-            boxShadow: isMobile ? "1px 1px 0 #000" : "2px 2px 0 #000",
-          }}
-        >
-          <span
-            className="text-lg sm:text-xl md:text-3xl font-bold text-white block text-center"
-            style={{
-              textShadow: "2px 2px 0 #3F1508",
-            }}
-          >
-            BCA Rp 1
-          </span>
-          <div className="mt-1 text-center px-1">
-            <Button
-              className="text-xs sm:text-sm font-bold bg-yellow-400 hover:bg-yellow-500 rounded-md px-2 py-1 font-crunch-chips border border-black"
-              style={{
-                textShadow: "1px 1px 0 #3F1508",
-                boxShadow: "1px 1px 0 #000",
-              }}
-            >
-              CLICK HERE
-            </Button>
-          </div>
-        </div>
+        <ButtonStyled limitWidth isMobile={isMobile} label="BCA Rp 1" />
 
         {/* Ticket options in a row */}
-        <div className="flex flex-col sm:flex-row justify-center space-y-3 sm:space-y-0 sm:space-x-3 md:space-x-4 w-full max-w-[90%] sm:max-w-[85%] md:max-w-[80%]">
+        <div className="flex flex-col sm:flex-row md:gap-6  justify-center space-y-3 sm:space-y-0 sm:space-x-3 w-full max-w-[90%] sm:max-w-[85%] md:max-w-[65%]">
           {/* General admission */}
-          <div
-            className="bg-[#EC497F] rounded-lg px-4 py-2 sm:px-5 sm:py-2 md:px-8 shadow-md w-full sm:w-1/2 border-[1px] sm:border-2 border-black"
-            style={{
-              boxShadow: isMobile ? "1px 1px 0 #000" : "2px 2px 0 #000",
-            }}
-          >
-            <span
-              className="block text-lg sm:text-xl md:text-3xl font-bold text-white text-center"
-              style={{
-                textShadow: "2px 2px 0 #3F1508",
-              }}
-            >
-              GENERAL
-              <br className="hidden sm:block" /> ADMISSION
-            </span>
-            <div className="mt-1 text-center px-1">
-              <Button
-                className="text-xs sm:text-sm font-bold bg-yellow-400 hover:bg-yellow-500 rounded-md px-2 py-1 font-crunch-chips border border-black"
-                style={{
-                  textShadow: "1px 1px 0 #3F1508",
-                  boxShadow: "1px 1px 0 #000",
-                }}
-              >
-                CLICK HERE
-              </Button>
-            </div>
-          </div>
-
-          {/* Private session */}
-          <div
-            className="bg-[#EC497F] rounded-lg px-4 py-2 sm:px-5 sm:py-2 md:px-8 shadow-md w-full sm:w-1/2 border-[1px] sm:border-2 border-black"
-            style={{
-              boxShadow: isMobile ? "1px 1px 0 #000" : "2px 2px 0 #000",
-            }}
-          >
-            <span
-              className="text-lg sm:text-xl md:text-3xl font-bold text-white block text-center"
-              style={{
-                textShadow: "2px 2px 0 #3F1508",
-              }}
-            >
-              PRIVATE SESSION WITH
-              <br className={isMobile ? "hidden" : "block"} />
-              {isMobile ? " " : ""} JACKSON GALAXY
-            </span>
-            <div className="mt-1 text-center px-1">
-              <Button
-                className="text-xs sm:text-sm font-bold bg-yellow-400 hover:bg-yellow-500 rounded-md px-2 py-1 font-crunch-chips border border-black"
-                style={{
-                  textShadow: "1px 1px 0 #3F1508",
-                  boxShadow: "1px 1px 0 #000",
-                }}
-              >
-                CLICK HERE
-              </Button>
-            </div>
-          </div>
+          <ButtonStyled isMobile={isMobile} label="GENERAL ADMISSION" />
+          <ButtonStyled
+            isMobile={isMobile}
+            label={
+              <>
+                PRIVATE SESSION WITH
+                <br className={isMobile ? "hidden" : "block"} />
+                {isMobile ? " " : ""} JACKSON GALAXY
+              </>
+            }
+          />
         </div>
       </div>
     </>
